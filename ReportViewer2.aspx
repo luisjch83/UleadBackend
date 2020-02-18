43fe6a8615d71e06060493e82fa1640c1107d1fd
luisjch83 <53988116+luisjch83@users.noreply.github.com>
2020-02-18 11:34:42 -0600

    Add files via upload

---
+++ ReportViewer2.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReportViewer2.aspx.cs" Inherits="EnvioPayLips.ReportViewer2" %>
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %><%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %> 
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form2" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+                <scripts>
+                    <asp:ScriptReference Assembly="ReportViewerForMvc" Name="ReportViewerForMvc.Scripts.PostMessage.js" />
+                </scripts>
+            </asp:ScriptManager>
+            <rsweb:ReportViewer ID="ReportViewer1" runat="server">
+            </rsweb:ReportViewer>
+        </div>
+    </form>
+    
+</body>
+</html>
---
+++ ReportViewer2.aspx.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EnvioPayLips
+{
+    public partial class ReportViewer2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShowReport();
+        }
+
+
+
+        private void ShowReport()
+        {
+            try
+            {
+                NetworkCredential nwc = new NetworkCredential("administrador", "$SCP.1987!*", "SCP");
+                WebClient client = new WebClient();
+                client.Credentials = nwc;
+                //report url  
+                string urlReportServer = "http://10.0.1.7";
+
+                // ProcessingMode will be Either Remote or Local  
+                ReportViewer1.ProcessingMode = ProcessingMode.Remote;
+
+                //Set the ReportServer Url  
+                ReportViewer1.ServerReport.ReportServerUrl = new Uri(urlReportServer);
+
+                // setting report path  
+                //Passing the Report Path with report name no need to add report extension   
+                ReportViewer1.ServerReport.ReportPath = "http://10.0.1.7/Reports/Pages/Folder.aspx/Reportes_Desarrollados/ADP_SSRR/SLP_CUSTOM/Reporte_05_SLP_AKamai";
+
+                //Set report Parameter  
+                //Creating an ArrayList for combine the Parameters which will be passed into SSRS Report  
+                //ArrayList reportParam = new ArrayList();  
+                //reportParam = ReportDefaultPatam();  
+
+                //ReportParameter[] param = new ReportParameter[reportParam.Count];  
+                //for (int k = 0; k < reportParam.Count; k++)  
+                //{  
+                //    param[k] = (ReportParameter)reportParam[k];  
+                //}  
+
+                // pass credential as if any... no need to pass anything if we use windows authentication  
+                //rptViewer.ServerReport.ReportServerCredentials =   
+                //  new ReportServerCredentials("UserName", "Password", "domainname");  
+
+                //pass parameters to report  
+                //rptViewer.ServerReport.SetParameters(param);   
+                ReportViewer1.ServerReport.Refresh();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
